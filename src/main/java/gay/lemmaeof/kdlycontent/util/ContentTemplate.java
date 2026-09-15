cffb2ca6ec03525b2291ad35e93035147c7d3882
LemmaEOF <6722691+LemmaEOF@users.noreply.github.com>
2026-09-15 00:43:46 -0700

    call them parameters for less confusion

--- src/main/java/gay/lemmaeof/kdlycontent/util/ContentTemplate.java
+++ src/main/java/gay/lemmaeof/kdlycontent/util/ContentTemplate.java
@@ -11,37 +11,37 @@ import java.util.Map;
 public record ContentTemplate(Identifier id, KdlNode template) {
 
 	public KdlNode expandFor(Identifier otherId, KdlNode other) throws ParseException {
-		Map<String, KdlValue<?>> valueArgs = new HashMap<>();
-		Map<String, KdlNode> nodeArgs = KdlHelper.mapNodes(other.children());
+		Map<String, KdlValue<?>> valueParams = new HashMap<>();
+		Map<String, KdlNode> nodeParams = KdlHelper.mapNodes(other.children());
 		for (String key : other.properties().propertyNames()) {
 			if (key.equals("template")) continue;
-			valueArgs.put(key, other.getProperty(key).get());
+			valueParams.put(key, other.getProperty(key).get());
 		}
-		return expandNode(otherId, template, valueArgs, nodeArgs);
+		return expandNode(otherId, template, valueParams, nodeParams);
 	}
 
-	private KdlNode expandNode(Identifier otherId, KdlNode node, Map<String, KdlValue<?>> valueArgs, Map<String, KdlNode> nodeArgs) throws ParseException {
+	private KdlNode expandNode(Identifier otherId, KdlNode node, Map<String, KdlValue<?>> valueParams, Map<String, KdlNode> nodeParams) throws ParseException {
 		//if the whole node is an arg just fill it in wholesale + rename properly
-		if (node.type() != null && node.type().equals("argument")) {
+		if (node.type() != null && node.type().equals("parameter")) {
 			String argName = KdlHelper.getArg(node, 0, "");
 			if (argName.isEmpty()) {
-				throw new ParseException(otherId, "missing name for node argument in template" + id);
-			} else if (!nodeArgs.containsKey(argName)) {
-				throw new ParseException(otherId, "missing node argument " + argName + " for template " + id);
+				throw new ParseException(otherId, "missing name for node parameter in template" + id);
+			} else if (!nodeParams.containsKey(argName)) {
+				throw new ParseException(otherId, "missing node parameter " + argName + " for template " + id);
 			} else {
-				return nodeArgs.get(argName).mutate().name(node.name()).build();
+				return nodeParams.get(argName).mutate().name(node.name()).build();
 			}
 		}
-		//kinda expensive recursive deep copy with argument filling
+		//kinda expensive recursive deep copy with param filling
 		KdlNode.Builder builder = KdlNode.builder().name(node.name());
 		//fill positional args
 		for (KdlValue<?> arg : node.arguments()) {
-			if (arg.type() != null && arg.type().equals("argument")) {
+			if (arg.type() != null && arg.type().equals("parameter")) {
 				String argName = String.valueOf(arg.value());
-				if (!valueArgs.containsKey(argName)) {
-					throw new ParseException(otherId, "missing value argument " + argName + " for template " + id);
+				if (!valueParams.containsKey(argName)) {
+					throw new ParseException(otherId, "missing value parameter " + argName + " for template " + id);
 				} else {
-					KdlValue<?> argValue = valueArgs.get(argName);
+					KdlValue<?> argValue = valueParams.get(argName);
 					builder.argument(argValue);
 				}
 			} else {
@@ -51,12 +51,12 @@ public record ContentTemplate(Identifier id, KdlNode template) {
 		//fill keyword properties
 		for (String key : node.properties().propertyNames()) {
 			KdlValue<?> val = node.getProperty(key).get();
-			if (val.type() != null && val.type().equals("argument")) {
+			if (val.type() != null && val.type().equals("parameter")) {
 				String argName = String.valueOf(val.value());
-				if (!valueArgs.containsKey(argName)) {
-					throw new ParseException(otherId, "missing value argument " + argName + " for template " + id);
+				if (!valueParams.containsKey(argName)) {
+					throw new ParseException(otherId, "missing value parameter " + argName + " for template " + id);
 				} else {
-					KdlValue<?> argValue = valueArgs.get(argName);
+					KdlValue<?> argValue = valueParams.get(argName);
 					builder.property(key, argValue);
 				}
 			} else {
@@ -66,7 +66,7 @@ public record ContentTemplate(Identifier id, KdlNode template) {
 
 		for (KdlNode child : node.children()) {
 			//recursion time!
-			builder.child(expandNode(otherId, child, valueArgs, nodeArgs));
+			builder.child(expandNode(otherId, child, valueParams, nodeParams));
 		}
 
 		return builder.build();
