20a749f8a2e14376ba22a54cbcdf2bd4415c53f8
LemmaEOF <6722691+LemmaEOF@users.noreply.github.com>
2026-09-15 02:06:57 -0700

    add skeleton for interpolation bc it's too late to do it for real

--- src/main/java/gay/lemmaeof/kdlycontent/util/ContentTemplate.java
+++ src/main/java/gay/lemmaeof/kdlycontent/util/ContentTemplate.java
@@ -22,27 +22,38 @@ public record ContentTemplate(Identifier id, KdlNode template) {
 
 	private KdlNode expandNode(Identifier otherId, KdlNode node, Map<String, KdlValue<?>> valueParams, Map<String, KdlNode> nodeParams) throws ParseException {
 		//if the whole node is an arg just fill it in wholesale + rename properly
-		if (node.type() != null && node.type().equals("parameter")) {
-			String argName = KdlHelper.getArg(node, 0, "");
-			if (argName.isEmpty()) {
-				throw new ParseException(otherId, "missing name for node parameter in template" + id);
-			} else if (!nodeParams.containsKey(argName)) {
-				throw new ParseException(otherId, "missing node parameter " + argName + " for template " + id);
+		if (node.type() != null) {
+			if (node.type().equals("parameter")) {
+				String argName = KdlHelper.getArg(node, 0, "");
+				if (argName.isEmpty()) {
+					throw new ParseException(otherId, "missing name for node parameter in template" + id);
+				} else if (!nodeParams.containsKey(argName)) {
+					throw new ParseException(otherId, "missing node parameter " + argName + " for template " + id);
+				} else {
+					return nodeParams.get(argName).mutate().name(node.name()).build();
+				}
 			} else {
-				return nodeParams.get(argName).mutate().name(node.name()).build();
+				throw new ParseException(otherId, "unknown type tag" + node.type() + " on node " + node.name());
 			}
 		}
 		//kinda expensive recursive deep copy with param filling
 		KdlNode.Builder builder = KdlNode.builder().name(node.name());
 		//fill positional args
 		for (KdlValue<?> arg : node.arguments()) {
-			if (arg.type() != null && arg.type().equals("parameter")) {
-				String argName = String.valueOf(arg.value());
-				if (!valueParams.containsKey(argName)) {
-					throw new ParseException(otherId, "missing value parameter " + argName + " for template " + id);
+			if (arg.type() != null) {
+				if (arg.type().equals("parameter")) {
+					String argName = String.valueOf(arg.value());
+					if (!valueParams.containsKey(argName)) {
+						throw new ParseException(otherId, "missing value parameter " + argName + " for template " + id);
+					} else {
+						KdlValue<?> argValue = valueParams.get(argName);
+						builder.argument(argValue);
+					}
+				} else if (arg.type().equals("interpolate")) {
+					String valText = String.valueOf(arg.value());
+					builder.argument(interpolate(otherId, valText, valueParams));
 				} else {
-					KdlValue<?> argValue = valueParams.get(argName);
-					builder.argument(argValue);
+					throw new ParseException(otherId, "unknown type tag " + arg.type() + " on argument " + arg);
 				}
 			} else {
 				builder.argument(arg);
@@ -50,17 +61,24 @@ public record ContentTemplate(Identifier id, KdlNode template) {
 		}
 		//fill keyword properties
 		for (String key : node.properties().propertyNames()) {
-			KdlValue<?> val = node.getProperty(key).get();
-			if (val.type() != null && val.type().equals("parameter")) {
-				String argName = String.valueOf(val.value());
-				if (!valueParams.containsKey(argName)) {
-					throw new ParseException(otherId, "missing value parameter " + argName + " for template " + id);
+			KdlValue<?> prop = node.getProperty(key).get();
+			if (prop.type() != null) {
+				if (prop.type().equals("parameter")) {
+					String argName = String.valueOf(prop.value());
+					if (!valueParams.containsKey(argName)) {
+						throw new ParseException(otherId, "missing value parameter " + argName + " for template " + id);
+					} else {
+						KdlValue<?> argValue = valueParams.get(argName);
+						builder.property(key, argValue);
+					}
+				} else if (prop.type().equals("interpolate")) {
+					String valText = String.valueOf(prop.value());
+					builder.property(key, interpolate(otherId, valText, valueParams));
 				} else {
-					KdlValue<?> argValue = valueParams.get(argName);
-					builder.property(key, argValue);
+					throw new ParseException(otherId, "unknown type tag " + prop.type() + " on property " + key);
 				}
 			} else {
-				builder.property(key, val);
+				builder.property(key, prop);
 			}
 		}
 
@@ -70,5 +88,9 @@ public record ContentTemplate(Identifier id, KdlNode template) {
 		}
 
 		return builder.build();
+	}
+
+	private String interpolate(Identifier otherId, String toFill, Map<String, KdlValue<?>> valueParams) throws ParseException {
+		throw new ParseException(otherId, "interpolation NYI (shout at Lemma to get this fixed ASAP)");
 	}
 }
